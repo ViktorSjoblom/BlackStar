692ee42bf846c067206474ac1c81661ce03bf84a
Jesper <jesper.lundqvist@gmail.com>
2019-11-08 15:22:32 +0100

    Created a nav menu

---
+++ public/navbar.js
@@ -0,0 +1,12 @@
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+const prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
