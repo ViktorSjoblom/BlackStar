9c6dcb095822223e0eadad79ca7515c98f352b1b
Jesper <jesper.lundqvist@gmail.com>
2019-11-24 16:49:56 +0100

    Added a navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,41 +1,53 @@
 // Navbar
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = () =>  {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.querySelector('.nav-bar').style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.querySelector('.nav-bar').style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
 
 
-// Navbar Hamburger
-/* When you click at the hamburger, the menu appears */
-const hamburger = document.getElementById('hamburger-div');
-const background = document.getElementById('hamburger-menu');
-const closeButton = document.getElementById('close');
-
-console.log(hamburger);
 
 
 
 
-        hamburger.addEventListener('click', function() {
-          if (window.innerWidth >= 1200) {
-            background.classList.add('activeDesktop');
-          }else {
-            background.classList.add('activeMobile');
-          }
-        });
 
-        closeButton.addEventListener('click', function() {
-            if (window.innerWidth >= 1200) {
-              background.classList.remove('activeDesktop');
-            }else {
-              background.classList.remove('activeMobile');
-            }
-          });
+
+
+
+
+
+
+
+// // Navbar Hamburger
+// /* When you click at the hamburger, the menu appears */
+// const hamburger = document.getElementById('hamburger-div');
+// const background = document.getElementById('hamburger-menu');
+// const closeButton = document.getElementById('close');
+
+// console.log(hamburger);
+
+
+
+
+//         hamburger.addEventListener('click', function() {
+//           if (window.innerWidth >= 1200) {
+//             background.classList.add('activeDesktop');
+//           }else {
+//             background.classList.add('activeMobile');
+//           }
+//         });
+
+//         closeButton.addEventListener('click', function() {
+//             if (window.innerWidth >= 1200) {
+//               background.classList.remove('activeDesktop');
+//             }else {
+//               background.classList.remove('activeMobile');
+//             }
+//           });
