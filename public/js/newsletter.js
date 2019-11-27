c501c9febad250a68c3b55b065631538da6f919b
Jesper <jesper.lundqvist@gmail.com>
2019-11-27 17:49:40 +0100

    Added eventlisteners and fixed hero images

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -1,17 +1,20 @@
-const newsletterButton = document.querySelector('.newsletter-button');
+const newsletterButtons = document.querySelectorAll('.newsletter-button');
+
 const exitButton = document.querySelector('.exit-button');
 
-
-newsletterButton.addEventListener('click', () => {
-    const newsletter = document.querySelector('.newsletter-mobile');
-
-    newsletter.classList.add('newsletter-mobile-active');
-
-    if (newsletter.classList.contains('newsletter-mobile-active')) {
-        exitButton.addEventListener('click', () => {
-            newsletter.classList.remove('newsletter-mobile-active');
-        });
-    }
-
+newsletterButtons.forEach(newsletterButton => {
+    newsletterButton.addEventListener('click', () => {
+        const newsletter = document.querySelector('.newsletter-mobile');
+    
+        newsletter.classList.add('newsletter-mobile-active');
+    
+        if (newsletter.classList.contains('newsletter-mobile-active')) {
+            exitButton.addEventListener('click', () => {
+                newsletter.classList.remove('newsletter-mobile-active');
+            });
+        }
+    });
+    
 });
+
 
